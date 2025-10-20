d4f9f06380bf1b0ffa60f10917542b03b8e0b62c
Moonsploit <155044100+Moonsploit@users.noreply.github.com>
2025-10-20 10:57:09 +0100

    Update factory_install.sh

--- builder/root/usr/sbin/factory_install.sh
+++ builder/root/usr/sbin/factory_install.sh
@@ -1,108 +1,71 @@
-#!/bin/bash
-# KVS: Kernel Version Switcher
-# Written by kxtzownsu / kxtz#8161
-# https://kxtz.dev
-# Licensed under GNU Affero GPL v3
-
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-    echo "$0 $(printf '\033[1;31mMUST\033[0m') be ran as root/sudo!"
-    exit
-fi
-
-version=1
-GITHUB_URL="https://github.com/kxtzownsu/KVS"
-tpmver=$(tpmc tpmver)
-
-if [ "$tpmver" == "2.0" ]; then
-  tpmdaemon="trunksd"
-else
-  tpmdaemon="tscd"
-fi
-
-# give me thy kernver NOW
-case "$(crossystem tpm_kernver)" in
-  "0x00000000")
-    kernver="0"
-    ;;
-  "0x00010001")
-    kernver="1"
-    ;;
-  "0x00010002")
-    kernver="2"
-    ;;
-  "0x00010003")
-    kernver="3"
-    ;;
-  *)
-    panic "invalid-kernver"
-    ;;
-esac
-
-# detect if booted from usb boot or from recovery boot
-if [ "$(crossystem mainfw_type)" == "recovery" ]; then
-  source /usr/share/kvs/tpmutil.sh
-  source /usr/share/kvs/functions.sh
-  mkdir -p /mnt/state &2>1 /dev/null
-  mount /dev/disk/by-label/KVS /mnt/state
-  stop $tpmdaemon
-  clear
-elif [ "$(crossystem mainfw_type)" == "developer" ]; then
-  source /usr/sbin/kvs/tpmutil.sh
-  source /usr/sbin/kvs/functions.sh
-  # panic "non-reco"
-  # sleep infinity
-  clear
-  . ../share/kvs/functions.sh
-  . ../share/kvs/tpmutil.sh
-  source ../share/kvs/functions.sh
-  source ../share/kvs/tpmutil.sh
-  style_text "YOU ARE RUNNING A DEBUG VERSION OF KVS, THIS WAS OPTIMIZED TO RUN ON CHROMEOS ONLY! ALL ACTIONS ARE PURELY VISUAL AND NOT FUNCTIONAL IN THIS MODE!!!"
-  sleep 5
-  clear
-fi
-
-credits(){
-  clear
-  echo "KVS: Kernel Version Switcher v$version"
-  echo "Current kernver: $kernver"
-  echo "TPM Version: $tpmver"
-  echo "TPMD: $tpmdaemon"
-  echo "-=-=-=-=-=-=-=-=-=-=-"
-  echo "kxtzownsu - Writing KVS, Providing kernver 0 & kernver 1 files."
-  echo "planetearth1363 - Providing kernver 2 files."
-  echo "miimaker - Providing kernver 3 files."
-  echo "OlyB - Helping me figure out the shim builder, seriously, thanks."
-  echo "Google - Writing the 'tpmc' command :3"
-  echo "-=-=-=-=-=-=-=-=-=-=-"
-  echo "Press ENTER to return to the main menu"
-  read -r
+#!/bin/sh
+# written mostly by HarryJarry1
+# get_stateful take from https://github.com/applefritter-inc/BadApple-icarus
+fail(){
+	printf "$1\n"
+	printf "exiting...\n"
+	exit
 }
-
-endkvs(){
-  # reboot now
-  stopwatch
-}
-
-
 main(){
-  echo "KVS: Kernel Version Switcher v$version"
-  echo "Current kernver: $kernver"
-  echo "TPM Version: $tpmver"
-  echo "TPMD: $tpmdaemon"
-  echo "-=-=-=-=-=-=-=-=-=-=-"
-  echo "1) Set New kernver"
-  echo "2) Backup kernver"
-  echo "3) Bash Shell"
-  echo "4) Credits"
-  echo "5) Exit"
-  printf '\x1b[?25h'
-  read -rep "$(printf '\x1b[?25h')> " sel
-  
-  selection $sel
+echo   
+get_internal
+mkdir /localroot
+mount "$intdis$intdis_prefix"3 /localroot -o ro
+mount --bindable /dev /localroot/dev
+chroot /localroot cgpt add "$intdis" -i 2 -P 10 -T 5 -S 1
+    (
+        echo "d"
+        echo "4"
+        echo "d"
+        echo "5"
+
+        echo "w"
+    ) | chroot /localroot fdisk "$intdis" 2>/dev/null
+umount /localroot/dev
+umount /localroot
+rmdir /localroot
+crossystem disable_dev_request=1
+mount "$intdis$intdis_prefix"1 /stateful || mountlvm
+rm -rf /stateful/*
+umount /stateful
+echo "Done!  Run reboot -f to reboot."
 }
-
-
-while true; do
-  clear
-  main
-done
+mountlvm(){
+     vgchange -ay #active all volume groups
+     volgroup=$(vgscan | grep "Found volume group" | awk '{print $4}' | tr -d '"')
+     echo "found volume group:  $volgroup"
+     mount "/dev/$volgroup/unencrypted" /stateful || fail "couldnt mount p1 or lvm group.  Please recover"
+}
+get_internal() {
+	# get_largest_cros_blockdev does not work in BadApple.
+	local ROOTDEV_LIST=$(cgpt find -t rootfs) # thanks stella
+	if [ -z "$ROOTDEV_LIST" ]; then
+		fail "could not parse for rootdev devices. this should not have happened."
+	fi
+	local device_type=$(echo "$ROOTDEV_LIST" | grep -oE 'mmc|nvme|sda' | head -n 1)
+	case $device_type in
+	"mmc")
+		intdis=/dev/mmcblk0
+  		intdis_prefix="p"
+		break
+		;;
+	"nvme")
+		intdis=/dev/nvme0
+  		intdis_prefix="n"
+		break
+		;;
+	"sda")
+		intdis=/dev/sda
+  		intdis_prefix=""
+		break
+		;;
+	*)
+		fail "an unknown error occured. this should not have happened."
+		;;
+	esac
+}
+read -p "are you sure you want to run daub?  (y/n) " -n 1 -r
+echo   
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    main
+fi
